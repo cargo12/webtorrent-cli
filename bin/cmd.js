fe7ce5b6a1e5a691e4aaf70456c3e679af6a72cc
Brad Marsden <silentbot1@gmail.com>
2018-06-09 18:22:36 +0100

    Implements file selection.

--- bin/cmd.js
+++ bin/cmd.js
@@ -346,6 +346,7 @@ function runDownload (torrentId) {
   var torrent = client.add(torrentId, { path: argv.out, announce: argv.announce })
 
   torrent.on('infoHash', function () {
+    torrent.so = argv.select.toString()
     if (argv.quiet) return
     updateMetadata()
     torrent.on('wire', updateMetadata)
